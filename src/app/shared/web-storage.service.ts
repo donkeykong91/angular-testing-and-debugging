4f3c97d3ce297a472c766ae7e6f1d8ce8b08e0dd
donkeykong91 <darylnator@gmail.com>
2020-04-15 06:48:52 -0700

    added a new webstorage service

---
+++ src/app/shared/web-storage.service.ts
@@ -0,0 +1,9 @@
+class XyzWebStorageService {
+    get (key: string) {
+        return window.localStorage.getItem(key);
+    }
+
+    set(key: string, value: string) {
+        window.localStorage.setItem(key, value);
+    }
+}
