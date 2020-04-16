bad0d8091976ac4cc790643f4bf6a4ca96b98f02
donkeykong91 <darylnator@gmail.com>
2020-04-16 06:49:50 -0700

    added new error service to replace default angular error service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -6,10 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 import { XyzUserListComponent } from './user-list/user-list.component';
+import { XyzErrorHandlerService } from './shared/error-handler.service';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule, HttpClientModule, routing ],
+  imports: [ BrowserModule, FormsModule, HttpClientModule, routing, ErrorHandler ],
   declarations: [ AppComponent, XyzUserListComponent ],
   bootstrap: [ AppComponent ],
+  providers: [
+    {provide: ErrorHandler, useClass: XyzErrorHandlerService}
+  ]
 })
 export class AppModule { }
