6785c70df2768523e7a152dd4ab9134505a28c1a
donkeykong91 <darylnator@gmail.com>
2020-04-16 06:06:36 -0700

    added new funcs for getting and setting remote data to and fro a db and making the service injectable

--- src/app/shared/web-storage.service.ts
+++ src/app/shared/web-storage.service.ts
@@ -1,4 +1,9 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+
+@Injectable()
 export class XyzWebStorageService {
+    constructor(private http: Http) {}
 
     get(key: string) {
         return window.localStorage.getItem(key);
@@ -6,5 +11,13 @@ export class XyzWebStorageService {
 
     set(key: string, value: string) {
         window.localStorage.setItem(key, value);
+    }
+
+    getRemote(){
+        return this.http.get<any>('http://localhost:5984/user/settings');
+    }
+
+    setRemote(payload: Object) {
+        return this.http.put<any>('http://localhost:5984/user/settings', payload);
     }
 }
