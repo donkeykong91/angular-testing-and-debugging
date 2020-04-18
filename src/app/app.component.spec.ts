ea73f0b5d42640e5ca13200d4a1854719b8ebae7
donkeykong91 <darylnator@gmail.com>
2020-04-18 04:29:31 -0700

    added component unit test

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,45 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { XyzUserListComponent } from './user-list/user-list.component';
+
+import { XyzErrorHandlerService } from './shared/error-handler.service';
+
+import {TestBed, ComponentFixture} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {DebugElement} from '@angular/core';
+
+import {APP_BASE_HREF} from '@angular/common';
+
+describe('AppComponent', () => {
+    let component: ComponentFixture<AppComponent>;
+    let element: DebugElement;
+    let html: HTMLElement;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ BrowserModule, FormsModule, HttpClientModule, routing ],
+            declarations: [ AppComponent, XyzUserListComponent ],
+            providers: [
+                {provide: APP_BASE_HREF, useValue: '/'}
+            ]
+        })
+
+        component = TestBed.createComponent(AppComponent);
+    });
+
+    it('should have a new title', () => {
+        let title = 'Users Online Now';
+
+        component.detectChanges();
+        element = component.debugElement.query(By.css('h2'));
+        html = element.nativeElement;
+
+        expect(html.innerText).toBe(title);
+    });
+
+})
